b82c866e8f054ca5588e504a2d8bfb4cb7a7f7a3
sunil <sunilssaharan@gmail.com>
2017-10-03 04:45:15 +0530

    added routes still need to use routes so that after a time lag of 3 seconds site willopen up

---
+++ client/public/js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('chat', [])
+  .factory('GUI', function() {
+    return require('nw.gui');
+  })
+  .factory('Window', ['GUI', function(gui) {
+    return gui.Window.get();
+  }])
+  .controller('Toolbar', ['$scope', 'Window',
+    function($scope, Window) {
+      /*
+      
+      Custom Control for Window operations
+
+      $scope.minimize = function() {
+        Window.minimize();
+      };
+
+      $scope.toggleFullscreen = function() {
+        Window.toggleKioskMode();
+      };
+
+      $scope.close = function() {
+        Window.close();
+      };*/
+    }
+  ]);
