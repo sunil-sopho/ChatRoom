e32aa3ce78588bcece977141e9e4d073f3febf56
Manish baghel <baghelmanish660@gmail.com>
2018-01-10 18:36:26 +0530

    cc

--- client/public/js/admin.js
+++ client/public/js/admin.js
@@ -68,28 +68,28 @@ function getCookie(cname) {
               socket.emit('newUser', {room:id,id: sessionId, name: $('#name').val()});
               });
               id = data + '_' + Math.random().toString(36).substr(2, 9);
-              $(".sideBar").prepend(
-                '<div class="row sideBar-body">'+
-                  '<div class="col-sm-3 col-xs-3 sideBar-avatar">'+
-                    '<div class="avatar-icon">'+
-                      '<img src="https://bootdey.com/img/Content/avatar/avatar1.png">'+
-                    '</div>'+
-                  '</div>'+
-                  '<div class="col-sm-9 col-xs-9 sideBar-main">'+
-                    '<div class="row">'+
-                      '<div class="col-sm-8 col-xs-8 sideBar-name">'+
-                        '<span class="name-meta">'+'John Doe'+
-                      '</span>'+
-                      '</div>'+
-                      '<div class="col-sm-4 col-xs-4 pull-right sideBar-time">'+
-                        '<span class="time-meta pull-right">18:18'+
-                      '</span>'+
-                      '</div>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'
-              );
-              $(".sideBar-body:first").attr('id',id);
+              // $(".sideBar").prepend(
+              //   '<div class="row sideBar-body">'+
+              //     '<div class="col-sm-3 col-xs-3 sideBar-avatar">'+
+              //       '<div class="avatar-icon">'+
+              //         '<img src="https://bootdey.com/img/Content/avatar/avatar1.png">'+
+              //       '</div>'+
+              //     '</div>'+
+              //     '<div class="col-sm-9 col-xs-9 sideBar-main">'+
+              //       '<div class="row">'+
+              //         '<div class="col-sm-8 col-xs-8 sideBar-name">'+
+              //           '<span class="name-meta">'+'John Doe'+
+              //         '</span>'+
+              //         '</div>'+
+              //         '<div class="col-sm-4 col-xs-4 pull-right sideBar-time">'+
+              //           '<span class="time-meta pull-right">18:18'+
+              //         '</span>'+
+              //         '</div>'+
+              //       '</div>'+
+              //     '</div>'+
+              //   '</div>'
+              // );
+              // $(".sideBar-body:first").attr('id',id);
             }
 
           });
@@ -135,6 +135,28 @@ function getCookie(cname) {
     console.log("room switching initiated");
     room = data.room;
     var id = room + '_' + Math.random().toString(36).substr(2, 9);
+    // $(".sideBar").prepend(
+    //   '<div class="row sideBar-body">'+
+    //     '<div class="col-sm-3 col-xs-3 sideBar-avatar">'+
+    //       '<div class="avatar-icon">'+
+    //         '<img src="https://bootdey.com/img/Content/avatar/avatar1.png">'+
+    //       '</div>'+
+    //     '</div>'+
+    //     '<div class="col-sm-9 col-xs-9 sideBar-main">'+
+    //       '<div class="row">'+
+    //         '<div class="col-sm-8 col-xs-8 sideBar-name">'+
+    //           '<span class="name-meta">'+'John Doe'+
+    //         '</span>'+
+    //         '</div>'+
+    //         '<div class="col-sm-4 col-xs-4 pull-right sideBar-time">'+
+    //           '<span class="time-meta pull-right">18:18'+
+    //         '</span>'+
+    //         '</div>'+
+    //       '</div>'+
+    //     '</div>'+
+    //   '</div>'
+    // );
+    // $(".sideBar-body:first").attr('id',id);
     globalId = id;
   });
 
